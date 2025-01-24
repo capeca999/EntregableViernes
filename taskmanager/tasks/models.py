c8b3610197962330bb6cf7246f18673197c7c5f9
capeca999 <47533906+capeca999@users.noreply.github.com>
2025-01-24 09:35:54 +0100

    Creada Las migraciones
    Migraciones creadas y el modelo base, a mejorar si es posible

---
+++ taskmanager/tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Tasks(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    difficulty = models.CharField( max_length=15)
+    
